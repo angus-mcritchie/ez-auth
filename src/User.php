be3d2da27ce06998735a00fad7b6730cdd4e4b80
Angus McRitchie <angus@hobbiesdirect.com.au>
2022-08-23 16:43:22 +1000

    Added user role

--- src/User.php
+++ src/User.php
@@ -9,6 +9,7 @@ class User
 	public ?string $firstName = null;
 	public ?string $lastName = null;
 	public ?string $email = null;
+	public ?string $role = null;
 
 	/**
 	 * Pass the decoded JWT to the constructor \
@@ -18,6 +19,7 @@ class User
 	 * $payload->firstName string The user's first name \
 	 * $payload->lastName string The user's last name \
 	 * $payload->email string The user's email
+	 * $payload->role string The user's role
 	 * 
 	 * @param object $payload (see above)
 	 * @return void
@@ -25,12 +27,31 @@ class User
 	public function __construct(object $payload)
 	{
 		$this->id = (int) $payload->sub;
-		$optional = ['username', 'firstName', 'lastName', 'email'];
 
-		foreach ($optional as $key) {
-			if (isset($payload->$key)) {
+		foreach (['username', 'firstName', 'lastName', 'email', 'role'] as $key) {
+			if (isset($payload->$key) && gettype($payload->$key) === 'string') {
 				$this->$key = (string) $payload->$key;
 			}
 		}
+	}
+
+	public function roleIsOneOf(array $roles): bool
+	{
+		foreach ($roles as $role) {
+			if ($this->roleIs($role)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public function roleIs(string $role): bool
+	{
+		if (!isset($this->role)) {
+			return false;
+		}
+
+		return strtolower($this->role) === strtolower($role);
 	}
 }
